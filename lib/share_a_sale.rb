9d467100bbc4848f809bb6083380afb3b79b4556
Ignacio Galindo <email@joiggama.net>
2017-05-24 11:48:23 -0500

    Request date defaults to UTC time

--- lib/share_a_sale.rb
+++ lib/share_a_sale.rb
@@ -11,13 +11,13 @@ module ShareASale
   class Client < Struct.new(:merchant_id, :token, :api_secret)
     { banner_list: "bannerList", transaction_detail: "transactionDetail", reference: "reference" }.each do |method, api_action|
       class_eval <<-EORUBY, __FILE__, __LINE__
-        def #{method}(options = {}, date = Time.now)
+        def #{method}(options = {}, date = Time.now.utc)
           request('#{api_action}', options, date).execute!
         end
       EORUBY
     end
 
-    def request(action, options, date = Time.now)
+    def request(action, options, date = Time.now.utc)
       Request.new(merchant_id, token, api_secret, action, options, date)
     end
   end
